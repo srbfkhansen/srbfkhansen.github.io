828f7c1acc6981a125d4239c3383ca29d5f56fab
srbfkhansen <han16053@byui.edu>
2019-03-07 17:07:42 -0700

    Setting up Weekly assignment files - adding JSON Example page to home page

---
+++ assignments/lesson-9/scripts/hamburger.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Hamburger Menu Functionality
+ * 
+ * Ruth Hansen
+ * Date: 2/16/2019 * 
+ * 
+ * 
+ * 
+ */
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
