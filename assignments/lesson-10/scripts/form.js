f3377f686fd32208c20f507e31bb4dcb80001a1a
srbfkhansen <han16053@byui.edu>
2019-03-14 19:29:30 -0600

    Adding week 10 pages

---
+++ assignments/lesson-10/scripts/form.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Form Info
+ * 
+ * Ruth Hansen
+ * Date: 3/2/2019 * 
+ * 
+ * 
+ */
+
+
+function adjustSeverity(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
